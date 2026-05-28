02aff3fde9a82730dd83b198e669e91ecc178f4f
Daniela <crisang15243@gmail.com>
2026-05-28 15:16:08 -0500

    feat: add CrossOrigin to controllers-pedidos

--- src/main/java/com/examen/pedidos/controller/PedidoController.java
+++ src/main/java/com/examen/pedidos/controller/PedidoController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/pedidos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PedidoController {
 
     private final PedidoService pedidoService;
